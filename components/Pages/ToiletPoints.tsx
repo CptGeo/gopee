92a739975fae9099176244d5a25feac167d83bd9
CptGeo <georgekalyvianakis18@gmail.com>
2023-07-10 22:29:43 +0200

    Remove redundant imports, print coords on point click

--- components/Pages/ToiletPoints.tsx
+++ components/Pages/ToiletPoints.tsx
@@ -1,13 +1,11 @@
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-import {StyleSheet, View} from 'react-native';
-import {Fragment} from 'react';
+import React from 'react';
 import Point from '../Point';
 
 import data from '../../data.json';
 
 export default function ToiletPoints() {
   return (
-    <Fragment>
+    <React.Fragment>
       {data.map(item => {
         return (
           <Point
@@ -15,11 +13,10 @@ export default function ToiletPoints() {
             draggable={true}
             coordinates={item.coordinates}
             image={require('../../assets/toilet.png')}
-            title="I am a toilet"
-            description="This toilet is really amazing"
+            title={`Lat: ${item.coordinates.latitude} - Lon: ${item.coordinates.longitude}`}
           />
         );
       })}
-    </Fragment>
+    </React.Fragment>
   );
 }
