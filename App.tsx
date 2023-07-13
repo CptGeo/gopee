bcfe5d4580457df23d8543a04cb91ece9c0d8a8d
CptGeo <georgekalyvianakis18@gmail.com>
2023-07-13 02:01:25 +0200

    Create variable to hold app readiness

--- App.tsx
+++ App.tsx
@@ -1,24 +1,33 @@
 
-import { StyleSheet, View } from "react-native";
-import React from "react";
+import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
 
 import SplashScreen from "./components/SplashScreen/SplashScreen"; 
 import { Map } from "./components/Map/Map";
 
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState<boolean>(false);
+
   const styles = StyleSheet.create({
     mapView: {
       flex: 1
     }
   });
 
+  useEffect(() => {
+    // Simulate a small delay until the app is ready (e.g. loading dependencies)
+    setTimeout(() => {
+      setAppIsReady(true);
+    }, 3000);
+  }, []);
+
   return (
     <React.Fragment>
-      {/* Uncomment to see splash screen */}
-      <SplashScreen />
       <View style={styles.mapView}>
         <Map />
       </View>
+      <SplashScreen appIsReady={appIsReady}/>
     </React.Fragment>
   )
 }
